ac02bb8b1e15b673e4f2ddf7256f2773aaca7424
Shrinath Prabhu <shrinathprabhu.123@gmail.com>
2023-05-04 16:14:24 +0530

    Remove axios

--- lib/apis.ts
+++ lib/apis.ts
@@ -1,39 +1,56 @@
-import axios, { AxiosInstance } from 'axios'
 import { OUTLINE_TRACKING_UID } from './keys'
 import state from './state'
 import type { AnalyticsEvents, PageData, EventKind } from './types'
 
-let api: AxiosInstance
-
-function createApiInstance(baseURL: string, apiVersion: string) {
-  api = axios.create({
-    baseURL: new URL(`${apiVersion}/${state.getAnalyticsId()}`, baseURL).href,
-  })
+const api: {
+  baseUrl: string
+} = {
+  baseUrl: '',
 }
 
-async function getTrackingUid(): Promise<string> {
+function createApiInstance(baseURL: string, apiVersion: string) {
+  api.baseUrl = new URL(`${apiVersion}/${state.getAnalyticsId()}`, baseURL).href
+}
+
+function getBraveHeader() {
+  if ((window.navigator as any).brave) {
+    return new Headers({
+      'X-Browser-Brave': '1',
+    })
+  } else {
+    return new Headers({})
+  }
+}
+
+async function getTrackingUid() {
   const outlineTrackingUid = localStorage.getItem(OUTLINE_TRACKING_UID)
   if (outlineTrackingUid) {
     return outlineTrackingUid
   } else {
-    const { data } = await api.get('/id')
+    const res = await fetch(`${api.baseUrl}/id`, { method: 'GET' })
+    const data: { id: string } = await res.json()
     localStorage.setItem(OUTLINE_TRACKING_UID, data.id)
     return data.id
   }
 }
 
-async function getTrackingEvents(): Promise<AnalyticsEvents> {
-  const { data } = await api.get('/events')
+async function getTrackingEvents() {
+  const res = await fetch(`${api.baseUrl}/events`, { method: 'GET' })
+  const data: { events: AnalyticsEvents } = await res.json()
   return data.events
 }
 
 function trackEvent(eventType: EventKind, event: string, page?: PageData) {
   const uid = state.getTrackingUid()
-  api.post('/event', {
-    uid,
-    event,
-    type: eventType,
-    page,
+  fetch(`${api.baseUrl}/event`, {
+    method: 'POST',
+    body: JSON.stringify({
+      uid,
+      event,
+      type: eventType,
+      page,
+    }),
+    headers: getBraveHeader(),
   })
 }
 
@@ -43,11 +60,15 @@ function trackSession(
   endTimestamp: string
 ) {
   const uid = state.getTrackingUid()
-  api.post('/session', {
-    uid,
-    page,
-    visitedAt: startTimestamp,
-    leftAt: endTimestamp,
+  fetch(`${api.baseUrl}/session`, {
+    method: 'POST',
+    body: JSON.stringify({
+      uid,
+      page,
+      visitedAt: startTimestamp,
+      leftAt: endTimestamp,
+    }),
+    headers: getBraveHeader(),
   })
 }
 
