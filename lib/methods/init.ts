d349cfd90799352df4983dded32bdd67c1f21e94
shrinathprabhu <shrinathprabhu.123@gmail.com>
2023-11-28 20:43:57 +0530

    Fix the condition for `isAlreadyInitialized`

--- lib/methods/init.ts
+++ lib/methods/init.ts
@@ -16,33 +16,33 @@ import { getRandomValue } from '@/methods/randomValues'
 let isAlreadyInitialized = false
 
 async function init(analyticsId: string, options?: InitOptions) {
-  state.setState({
-    analyticsId,
-    debug: options?.debug,
-    mock: options?.mock,
-    data: options?.data,
-  })
-  logger.log('Initialized with id ', `"${analyticsId}"`)
-
-  const apiVersion = options?.apiVersion || 'v1'
-  const serverUrl = options?.serverUrl || OUTLINE_API_ENDPOINT
-  createApiInstance(serverUrl, apiVersion)
-
-  const persistedVisitorUid = localStorage.getItem(OUTLINE_VISITOR_UID_KEY)
-  const visitorUid = persistedVisitorUid || `OAU-${getRandomValue()}`
-  if (!persistedVisitorUid)
-    localStorage.setItem(OUTLINE_VISITOR_UID_KEY, visitorUid)
-
-  let sessionId = sessionStorage.getItem(PAGE_SESSION_KEY)
-  const events = await getTrackingEvents()
-  if (!sessionId) sessionId = generateNewSessionId()
-  state.setState({
-    visitorUid,
-    sessionId,
-    analyticsEvents: events,
-  })
-
   if (!isAlreadyInitialized) {
+    state.setState({
+      analyticsId,
+      debug: options?.debug,
+      mock: options?.mock,
+      data: options?.data,
+    })
+    logger.log('Initialized with id ', `"${analyticsId}"`)
+
+    const apiVersion = options?.apiVersion || 'v1'
+    const serverUrl = options?.serverUrl || OUTLINE_API_ENDPOINT
+    createApiInstance(serverUrl, apiVersion)
+
+    const persistedVisitorUid = localStorage.getItem(OUTLINE_VISITOR_UID_KEY)
+    const visitorUid = persistedVisitorUid || `OAU-${getRandomValue()}`
+    if (!persistedVisitorUid)
+      localStorage.setItem(OUTLINE_VISITOR_UID_KEY, visitorUid)
+
+    let sessionId = sessionStorage.getItem(PAGE_SESSION_KEY)
+    const events = await getTrackingEvents()
+    if (!sessionId) sessionId = generateNewSessionId()
+    state.setState({
+      visitorUid,
+      sessionId,
+      analyticsEvents: events,
+    })
+
     startTracking()
     startPageSession()
     let pageLeftTime: number
@@ -80,6 +80,8 @@ async function init(analyticsId: string, options?: InitOptions) {
     sendDefaultEvent('internal', 'pageview')
     enableSPATracking()
     isAlreadyInitialized = true
+  } else if (options?.data) {
+    state.setState({ data: options.data })
   }
 
   return {
