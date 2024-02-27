d926861274fc8070b7ba20bedc898ca9c6f7e108
shrinathprabhu <shrinathprabhu.123@gmail.com>
2024-02-27 16:50:36 +0530

    Bump a new version with types support

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,10 @@
 import { init } from '@/methods/init'
+import type {
+  InitOptions,
+  AnalyticsEvent,
+  PageData,
+  AnalyticsEvents,
+} from '@/types'
 
 export default init
+export type { InitOptions, AnalyticsEvent, PageData, AnalyticsEvents }
